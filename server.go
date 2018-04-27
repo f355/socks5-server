1997cfca53587dff2efdc4b01d6c68d39cb4ed2b
Konstantin Tcepliaev <f355@f355.org>
2018-04-27 13:36:14 +0200

    Add authentication logging.

--- server.go
+++ server.go
@@ -7,14 +7,44 @@ import (
 	"log"
 )
 
+type LoggingCredentials struct {
+	UserPass map[string]string
+	Logger *log.Logger
+}
+
+func (c LoggingCredentials) Valid(user, password string) bool {
+	pass, ok := c.UserPass[user]
+	if !ok {
+		c.Logger.Printf("[WRN] unknown user: %v", user)
+		return false
+	}
+
+	if password == pass {
+		c.Logger.Printf("[INF] authenticated: %v", user)
+		return true
+	}
+
+	c.Logger.Printf("[WRN] wrong password for user: %v", user)
+	return false
+}
+
+
 func main() {
-	var creds socks5.StaticCredentials
-	json.Unmarshal([]byte(os.Getenv("PROXY_AUTH")), &creds)
+	var user_pass map[string]string
+	json.Unmarshal([]byte(os.Getenv("PROXY_AUTH")), &user_pass)
+
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	creds := LoggingCredentials{
+		UserPass: user_pass,
+		Logger: logger,
+	}
 	cator := socks5.UserPassAuthenticator{Credentials: creds}
+
 	// Create a SOCKS5 server
 	conf := &socks5.Config{
 		AuthMethods: []socks5.Authenticator{cator},
-		Logger:      log.New(os.Stdout, "", log.LstdFlags),
+		Logger:      logger,
 	}
 	server, err := socks5.New(conf)
 	if err != nil {
